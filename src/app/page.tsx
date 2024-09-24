0ae5c1919c05501d666db53cd5bc68638f6cce3f
Maharba07 <maharba1707@gmail.com>
2024-09-24 16:53:47 -0400

    Actualizacion Portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,21 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <h1 className="text-center text-3xl font-bold">My Portfolio</h1>
-      {/* Colapse del About Me! */}
       <div className="collapse bg-base-200">
         <input type="checkbox" />
-        <div className="collapse-title text-xl font-medium">About Me!</div>
+        <div className="collapse-title text-xl font-medium">About Me! 😊</div>
         <div className="collapse-content">
           <p className="text-2xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam totam,
-            perspiciatis illum dolores, architecto hic ullam cupiditate nemo
-            dicta accusantium nostrum, impedit culpa dolor molestiae pariatur
-            asperiores voluptatibus sit placeat.
+          ¡Hola! Soy un apasionado diseñador y desarrollador web 💻. Me encanta crear diseños modernos e interactivos, con un enfoque en la experiencia del usuario 🎨. ¡Trabajemos juntos para construir algo increíble! 🚀
+          </p>
+          <div className="divider divider-info text-2xl font-extrabold"> </div>
+          <p className="text-2xl">
+            Hi! I'm a passionate web designer and developer 💻. I love creating
+            modern and interactive designs with a focus on user experience 🎨.
+            Let’s work together to build something amazing! 🚀
           </p>
         </div>
       </div>
-      {/* Imagen de Perfil */}
       <div className="avatar">
         <div className="w-40 rounded-full">
           <Image src={image} alt="Imagen de perfil" />
@@ -27,13 +28,14 @@ export default function Home() {
           corporis dicta soluta
         </p> */}
       </div>
-      <p className="text 2xl text-end">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae corporis
-        dicta soluta
-      </p>
+      <div className="text-center mt-4">
+    <h2 className="text-2xl font-bold">Abraham García</h2>
+    <p className="text-lg">
+      Desarrollador web apasionado por el diseño moderno y las experiencias interactivas.
+    </p>
+  </div>
       {/* Boton para el modo oscuro */}
       <label className="swap swap-rotate bottom-40 place-self-start">
-        {/* this hidden checkbox controls the state */}
         <input type="checkbox" className="theme-controller" value="cupcake" />
 
         {/* sun icon */}
@@ -58,16 +60,83 @@ export default function Home() {
         <div className="divider divider-primary text-2xl font-extrabold">
           Proyectos
         </div>
+
+        {/* Carrusel de proyectos */}
+        <div className="carousel w-full rounded-lg shadow-lg">
+          <div id="slide1" className="carousel-item relative w-full">
+            <img
+              src="/images/url_shortner.png"
+              className="h-64 w-full object-cover"
+              alt="Proyecto 1"
+            />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide4" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide2" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide2" className="carousel-item relative w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+              className="h-64 w-full object-cover"
+              alt="Proyecto 2"
+            />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+              className="h-64 w-full object-cover"
+              alt="Proyecto 3"
+            />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide2" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide4" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide4" className="carousel-item relative w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+              className="h-64 w-full object-cover"
+              alt="Proyecto 4"
+            />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide3" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide1" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+        </div>
+
         <div className="divider divider-info text-2xl font-extrabold">
           Contacto
         </div>
         <br />
-        <h2 className="text-blue-500 text-center text-xl font-medium">Email:</h2>
+        <h2 className="text-center text-xl font-medium text-blue-500">
+          Email:
+        </h2>
         <a
           className="link link-accent text-center font-extrabold"
           href="mailto:maharba1707@gmail.com?"
         >
-          maharba1707@gmail2.com
+          maharba1707@gmail.com
         </a>
       </div>
     </main>
